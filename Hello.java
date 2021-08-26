5cbdf2f324df1c51ef52e56ab105b1af3cb508af
kylehadj <89561088+kylehadj@users.noreply.github.com>
2021-08-25 22:32:33 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,12 @@
+public class Hello {
+	public static void main(String[] args) {
+
+		System.out.println("<Kyle Hadj-Ali>");
+		System.out.println("<Hello World>");
+		System.out.println("<Lab 01>");
+		System.out.println("<08/25/2021>");
+		System.out.println("<255-004>");
+		System.out.println();
+
+	}
+}
